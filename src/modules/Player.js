6722f67567677fb3a0fff83bf6d416a0105df9fb
Charles Kamiri <charleskamiri6@gmail.com>
2023-10-17 18:57:53 +0300

    Added basic movement for player class

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,17 +1,37 @@
+const gravity = 0.5;
+
 export default class Player {
-    constructor({position}) {
+    constructor({canvas, position}) {
+        this.canvas = canvas;
         this.position = position;
-        this.width = 100;
-        this.height = 100;
+        this.width = 50;
+        this.height = 50;
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
     };
 
     draw(context) {
-        context.fillStyle = '#f00';
+        context.fillStyle = 'blue';
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
     };
 
     update() {
+        // Horizontal movement
+        this.position.x += this.velocity.x
 
+        // Vertical movement
+        this.position.y += this.velocity.y;
+
+
+        // Surface boundary
+        if (this.position.y + this.height + this.velocity.y < this.canvas.height - 100) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
     };
 
     render(context) {
--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import Player from './modules/Player';
+
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas');
     /** @type {CanvasRenderingContext2D} */
@@ -5,4 +7,92 @@ document.addEventListener('DOMContentLoaded', () => {
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const keys = {
+        up: {pressed: false},
+        right: {pressed: false},
+        down: {pressed: false},
+        left: {pressed: false}
+    };
+
+    const player = new Player({
+        canvas: canvas,
+        position: {x: 100, y: 100}
+    });
+
+    const animate = () => {
+        requestAnimationFrame(animate);
+
+        // Render background
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Render surface
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, canvas.height - 100, canvas.width, canvas.height);
+
+        player.render(ctx);
+
+
+        // Player movement
+        if (keys.right.pressed) {
+            player.velocity.x = 10
+        } else if (keys.left.pressed) {
+            player.velocity.x = -10
+        } else {
+            player.velocity.x = 0;
+        }
+    };
+
+    animate();
+
+    // Controls
+    window.addEventListener('keydown', e => {
+        switch (e.key) {
+            case 'w':
+            case 'ArrowUp':
+                keys.up.pressed = true
+                player.velocity.y = -15;
+                break;
+
+            case 'd':
+            case 'ArrowRight':
+                keys.right.pressed = true;
+                break;
+
+            case 's':
+            case 'ArrowDown':
+                keys.down.pressed = true;
+                break;
+
+            case 'a':
+            case 'ArrowLeft':
+                keys.left.pressed = true;
+                break;
+        };
+    });
+
+    window.addEventListener('keyup', e => {
+      switch (e.key) {
+        case "w":
+        case "ArrowUp":
+          keys.up.pressed = false;
+          break;
+
+        case "d":
+        case "ArrowRight":
+          keys.right.pressed = false;
+          break;
+
+        case "s":
+        case "ArrowDown":
+          keys.down.pressed = false;
+          break;
+
+        case "a":
+        case "ArrowLeft":
+          keys.left.pressed = false;
+          break;
+      }
+    });
 });
